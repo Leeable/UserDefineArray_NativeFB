145cde862ee7895e5c0c125e548e5018cdb072a3
Lee qun zhao <65322941+Leeable@users.noreply.github.com>
2023-11-17 10:18:28 +0800

    Delete ArrayTestCSharp directory

--- ArrayTestCSharp/Cli/CreateDataType-template.cpp
+++
@@ -1,8 +0,0 @@
-// CreateDataType native assembly code template.
-// PHOENIX CONTACT Software embedded CLR native assembly builder tool generated source file.
-// use this file to implement your own native code
-
-#include "eclr.h"
-#include "ArrayTest.h"
-
-// class ArrayTest.CreateDataType implementation
