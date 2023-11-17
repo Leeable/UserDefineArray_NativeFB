08f773f5fb3d8362e4a054c7bc5b690203ed0c54
Lee qun zhao <65322941+Leeable@users.noreply.github.com>
2023-11-17 10:19:31 +0800

    Add files via upload

---
+++ ArrayTest/ArrayTestCSharp/Cli/TString600-template.cpp
@@ -0,0 +1,13 @@
+// TString600 native assembly code template.
+// PHOENIX CONTACT Software embedded CLR native assembly builder tool generated source file.
+// use this file to implement your own native code
+
+#include "eclr.h"
+#include "ArrayTest.h"
+
+// class ArrayTest.TString600 implementation
+void __PInvoke__ ArrayTest::TString600::Init()
+{
+    // implement your code here !
+}
+
