b500c4087ada6ea8b5f67e9aaadab00bcb126f3f
Lee qun zhao <65322941+Leeable@users.noreply.github.com>
2023-11-17 10:17:02 +0800

    Add files via upload

---
+++ ArrayTestCSharp/Cli/ArrayTest-version.h
@@ -0,0 +1,10 @@
+#ifndef __ARRAYTEST_VERSION_H__
+#define __ARRAYTEST_VERSION_H__
+// PHOENIX CONTACT Software eCLR Native Library Builder version 3.0.1033.0
+
+struct ArrayTestVersion
+{
+    enum Version { Major=1, Minor=0, Build=0, Revision=0 };
+};
+
+#endif //#ifndef __ARRAYTEST_VERSION_H__
---
+++ ArrayTestCSharp/Cli/IntArrayFB-template.cpp
@@ -0,0 +1,19 @@
+// IntArrayFB native assembly code template.
+// PHOENIX CONTACT Software embedded CLR native assembly builder tool generated source file.
+// use this file to implement your own native code
+
+#include "eclr.h"
+#include "ArrayTest.h"
+
+// class ArrayTest.IntArrayFB implementation
+ArrayTest::CreateDataType __PInvoke__ ArrayTest::IntArrayFB::get_Item(Int32 p0)
+{
+    // implement your code here !
+    return 0;
+}
+
+void __PInvoke__ ArrayTest::IntArrayFB::set_Item(Int32 p0, ArrayTest::CreateDataType p1)
+{
+    // implement your code here !
+}
+
