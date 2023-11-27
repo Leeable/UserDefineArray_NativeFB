c03361826cf0bbfbc1df66df4077d98e0b2a2d8f
Lee qun zhao <65322941+Leeable@users.noreply.github.com>
2023-11-27 16:55:33 +0800

    Update ArrayCplus.cs

--- ArrayTest/ArrayTestCSharp/ArrayCplus.cs
+++ ArrayTest/ArrayTestCSharp/ArrayCplus.cs
@@ -38,12 +38,6 @@ namespace ArrayTest
         public IecString80 sName;
         public cType sType;
         public IecString80 sAttriibute;
-        [Initialization]
-        public void __Init()
-        {
-            //  No implementation in C# part; implement in native method
-
-        }
     }
 
     [Native]
