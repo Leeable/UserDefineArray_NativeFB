1198146c9dcb31b97796000a507414eee186aa43
Lee qun zhao <65322941+Leeable@users.noreply.github.com>
2023-11-28 13:59:43 +0800

    Update CreateDataType-cli.cpp

--- ArrayTest/ArrayTestCpp/src/CreateDataType-cli.cpp
+++ ArrayTest/ArrayTestCpp/src/CreateDataType-cli.cpp
@@ -6,3 +6,14 @@
 #include "ArrayTest.h"
 
 // class ArrayTest.CreateDataType implementation
+void __PInvoke__ ArrayTest::CreateDataType::Init()
+{
+    // implement your code here !
+    this->sName.SetCapacity(this->sName._Capacity_);
+    this->sName.Empty();
+    this->sType = cType::sql_REAL;
+    this->sAttriibute.SetCapacity(this->sAttriibute._Capacity_);
+    this->sAttriibute.Empty();
+
+}
+
