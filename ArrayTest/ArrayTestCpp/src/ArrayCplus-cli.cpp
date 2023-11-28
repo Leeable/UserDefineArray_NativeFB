b3a04ee23bb04fe8162be9fa5361756a55b69dba
Lee qun zhao <65322941+Leeable@users.noreply.github.com>
2023-11-28 13:58:38 +0800

    Update ArrayCplus-cli.cpp

--- ArrayTest/ArrayTestCpp/src/ArrayCplus-cli.cpp
+++ ArrayTest/ArrayTestCpp/src/ArrayCplus-cli.cpp
@@ -4,29 +4,16 @@
 
 #include "eclr.h"
 #include "ArrayTest.h"
-int state = 0;
+
 // class ArrayTest.ArrayCplus implementation
 void __PInvoke__ ArrayTest::ArrayCplus::__Init()
 {
     // implement your code here !
-    
 }
 
 void __PInvoke__ ArrayTest::ArrayCplus::__Process()
 {
     // implement your code here !
-    if (!state)
-    {
-        CreateDataType Demo;
-        Demo.sName.Empty();
-        Demo.sAttriibute.Empty();
-        Demo.sType = cType::EM;
-        for (int i = 0; i <= 20; i++)
-        {
-            this->ArrayFB.set_Item(i, Demo); 
-        }
-        state = 1;
-    }
 }
 
 ArrayTest::ArrayCplus::ArrayCplus()
